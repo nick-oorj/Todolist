d528b3bb3c8a23870e07869d57da624647128a77
nick-oorj <qhrrr07@gmail.com>
2025-06-20 23:28:55 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EndAll from './mess/EndAll';
+import Todolistinteg from './mess/Todolistinteg';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Todolistinteg />
+      <EndAll />
+    </div>
+  )
+}
+
+export default App;
